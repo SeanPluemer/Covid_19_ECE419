d9d8d43cb51633539d6a89fdbe7515be26529928
SeanPluemer <58951141+SeanPluemer@users.noreply.github.com>
2020-10-16 12:27:24 -0600

    Updated the app to include a working graph

---
+++ app/src/main/java/com/example/covid19/LineChartActivity.java
@@ -0,0 +1,77 @@
+package com.example.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class LineChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line_chart);
+        setTitle("LineChartTime");
+        LineChart lineChart = findViewById(R.id.lineChart);
+
+        // no description text
+        lineChart.getDescription().setEnabled(false);
+
+        // enable touch gestures
+        lineChart.setTouchEnabled(true);
+
+        lineChart.setDragDecelerationFrictionCoef(0.9f);
+
+        // enable scaling and dragging
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(true);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setHighlightPerDragEnabled(true);
+
+        // set an alternative background color
+        lineChart.setBackgroundColor(Color.WHITE);
+        lineChart.setViewPortOffsets(0f, 0f, 0f, 0f);
+
+
+
+
+
+
+        ArrayList<Entry> visitors = new ArrayList<>();
+        visitors.add(new Entry(2014,420));
+        visitors.add(new Entry(2015,843));
+        visitors.add(new Entry(2016,526));
+        visitors.add(new Entry(2017,634));
+        visitors.add(new Entry(2018,235));
+        visitors.add(new Entry(2019,532));
+
+
+
+        LineDataSet lineDataSet = new LineDataSet(visitors,"visitors");
+        lineDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        lineDataSet.setValueTextColor(Color.BLACK);
+        lineDataSet.setValueTextSize(16f);
+
+        LineData lineData = new LineData(lineDataSet);
+
+        //lineChart.setFitBars(true);
+        lineChart.setData(lineData);
+        lineChart.getDescription().setText("Line Chart Example");
+        lineChart.animateX(2000);
+
+
+
+    }
+}
